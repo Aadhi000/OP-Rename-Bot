3d4c6733356685c645c7f31d99fa19df0b582d8f
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-03-06 13:17:36 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -9,16 +9,16 @@ from bot.core.db.add import add_user_to_database
 @Client.on_message(filters.command(["start", "ping"]) & filters.private & ~filters.edited)
 async def ping_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("I don't know about you sir :(")
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Hi, I am Rename Bot!\n\n"
-             "I can rename media without downloading it!\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.",
+        text="<b>𝙷𝙴𝚈 𝙸 𝙰𝙼 𝙾𝙿-𝚁𝙴𝙽𝙰𝙼𝙴 𝙱𝙾𝚃!</b>\n\n"
+             "<b>𝙸 𝙲𝙰𝙽 𝚁𝙴𝙽𝙰𝙼𝙴 𝙼𝙴𝙳𝙸𝙰 𝚆𝙸𝚃𝙷𝙾𝚄𝚃 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙸𝚃!</b>\n"
+             "<b>𝚂𝙿𝙴𝙴𝙳 𝙳𝙴𝙿𝙴𝙽𝙳𝚂 𝙾𝙽 𝚈𝙾𝚄𝚁 𝙼𝙴𝙳𝙸𝙰 𝙳𝙲.</b>\n\n"
+             "<b>𝙹𝚄𝚂𝚃 𝚂𝙴𝙽𝙳 𝙼𝙴 𝙼𝙴𝙳𝙸𝙰 𝙰𝙽𝙳 𝚁𝙴𝙿𝙻𝚈 𝙸𝚃 𝚆𝙸𝚃𝙷 /rename 𝙲𝙾𝙼𝙼𝙰𝙽𝙳.</b>",
         reply_markup=types.InlineKeyboardMarkup([[
-           types.InlineKeyboardButton("Show Settings",
+           types.InlineKeyboardButton("𝙱𝙾𝚃 𝚂𝙴𝚃𝚃𝙸𝙽𝙶𝚂",
                                       callback_data="showSettings")
         ]])
     )
@@ -31,12 +31,12 @@ async def help_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="I can rename media without downloading it!\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.\n\n"
-             "To set custom thumbnail reply to any image with /set_thumbnail\n\n"
-             "To see custom thumbnail press /show_thumbnail",
+        text="<b>𝙸 𝙲𝙰𝙽 𝚁𝙴𝙽𝙰𝙼𝙴 𝙼𝙴𝙳𝙸𝙰 𝚆𝙸𝚃𝙷𝙾𝚄𝚃 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙸𝚃!</b>\n"
+             "<b>𝚂𝙿𝙴𝙴𝙳 𝙳𝙴𝙿𝙴𝙽𝙳𝚂 𝙾𝙽 𝚈𝙾𝚄𝚁 𝙼𝙴𝙳𝙸𝙰 𝙳𝙲.</b>\n\n"
+             "<b>𝙹𝚄𝚂𝚃 𝚂𝙴𝙽𝙳 𝙼𝙴 𝙼𝙴𝙳𝙸𝙰 𝙰𝙽𝙳 𝚁𝙴𝙿𝙻𝚈 𝙸𝚃 𝚆𝙸𝚃𝙷 /rename 𝙲𝙾𝙼𝙼𝙰𝙽𝙳.</b>\n\n"
+             "<b>𝚃𝙾 𝚂𝙴𝚃 𝙲𝚄𝚂𝚃𝙾𝙼 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰𝙽𝚈 𝙸𝙼𝙰𝙶𝙴 𝚆𝙸𝚃𝙷 /set_thumbnail</b>\n\n"
+             "<b>𝚃𝙾 𝚂𝙴𝙴 𝙲𝚄𝚂𝚃𝙾𝙼 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝙿𝚁𝙴𝚂𝚂 /show_thumbnail</b>",
         reply_markup=types.InlineKeyboardMarkup([[
-           types.InlineKeyboardButton("Show Settings",
+           types.InlineKeyboardButton("𝙱𝙾𝚃 𝚂𝙴𝚃𝚃𝙸𝙽𝙶𝚂",
                                       callback_data="showSettings")]])
     )
