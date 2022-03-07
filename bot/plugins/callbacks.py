5d635f34af19a9adef8d0af7ec2d7a2b908f248e
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-03-08 00:00:40 +0530

    Update callbacks.py

--- bot/plugins/callbacks.py
+++ bot/plugins/callbacks.py
@@ -50,7 +50,7 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
                                                                    callback_data="showSettings")]],
                                   reply_markup=types.InlineKeyboardMarkup(
                                       [[types.InlineKeyboardButton("✅ 𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ✅", url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')]]
-                                  ))
+                                  )
     elif cb.data == "setCustomCaption":
         await cb.answer()
         await cb.message.edit("Okay,\n"
@@ -70,7 +70,7 @@ async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
                                                                    callback_data="showSettings")]],
                                   reply_markup=types.InlineKeyboardMarkup(
                                       [[types.InlineKeyboardButton("✅ 𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ✅", url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')]]
-                                  ))
+                                  )
     elif cb.data == "triggerApplyCaption":
         await cb.answer()
         apply_caption = await db.get_apply_caption(cb.from_user.id)
